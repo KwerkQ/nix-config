d016e108ad212c785e39aac597b674fcd47f4bac
KwerkQ <filasimonov4@gmail.com>
2026-06-30 10:11:25 +0300

    some rewrite things

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,6 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-      
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -46,22 +45,12 @@
     LC_TIME = "en_GB.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  # services.displayManager.sddm.enable = true;
-  services.displayManager.ly.enable = true;
-  #services.desktopManager.plasma6.enable = true;
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
   # Enable sound with pipewire.
@@ -75,52 +64,45 @@
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Experimental Features
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users."flip" = {
     shell = pkgs.fish;
     isNormalUser = true;
     description = "flip";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    ];
+    packages = with pkgs; [];
   };
-
-  # Install and enable all kinds of stuff.
-  programs.firefox.enable = true;
-  programs.sway.enable = true;
-  programs.fish.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # Experimental features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+
   fonts.packages = with pkgs; [
 	nerd-fonts.jetbrains-mono
   ];
+
+  # Enable some stuff
+  programs.firefox.enable = true;
+  programs.sway.enable = true;
+  programs.fish.enable = true;
+  services.displayManager.ly.enable = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   wget
-  fastfetch
   git
-  fsel
-  i3status
   alacritty
-  eza
+  i3status
   swayfx
-  #zoxide
+  fastfetch
+  eza
+  fsel
+  fastfetch
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
