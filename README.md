56ee37c4d30d7d52f21e17964208081d5aea235c
KwerkQ <filasimonov4@gmail.com>
2026-06-30 17:10:30 +0300

    added a setup.bash file and updated readme

---
+++ README.md
@@ -0,0 +1,1 @@
+The setup file assumes that you git cloned this repo to ~/nix-config of your main user. Do not execute the bash script in nix-shell.
--- setup.bash
+++ setup.bash
@@ -2,12 +2,13 @@
 
 sudo rm -rf /etc/nixos
 echo removed /etc/nixos
-nix-shell -p git
-echo created a shell with git
-cd ~
-git clone https://github.com/kwerkq/nix-config
-echo git cloned github.com/kwerkq/nix-config
+
 sudo ln -s ~/nix-config /etc/nixos
 echo soft linked ~/nix-config and /etc/nixos
+
 sudo nixos-rebuild switch --flake ~/nix-config/#nixos-btw
 echo rebuilt nix with the flake in ~/nix-config
+
+git clone --quiet https://github.com/kwerkq/newdots ~/.config/
+
+git clone --quiet https://github.com/orangci/walls-catppuccin-mocha ~/Pictures/Wallpapers/walls-catppuccin-mocha/
